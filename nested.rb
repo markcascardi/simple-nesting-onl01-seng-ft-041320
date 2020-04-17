f11e7ee601550be010616eb36d0a4d28747eb05e
Mark Cascardi <mcascardi@gmail.com>
2020-04-17 19:46:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 programmer_hash[:alan_kay] = {:known_for => "GUI"}
-
 programmer_hash
 end
 
@@ -125,5 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash
 
 end
